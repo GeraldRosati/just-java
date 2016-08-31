1b5536b874594db1b6621f5c03efaba2c4ff014d
Gerald Rosati <gci6@wildcats.unh.edu>
2016-08-30 20:38:48 -0400

    Changed displayed text when the order button is pressed.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -13,28 +13,56 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    public void increment(View view) {
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    public void decrement(View view) {
+
+        // Do not let the quantity become negative
+        if (quantity > 0) {
+            quantity = quantity - 1;
+        }
+
+        display(quantity);
+    }
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 5;
+        // displayPrice(quantity * 5);
+        String priceMessage = "Total: $" + (quantity * 5);
+        priceMessage = priceMessage + "\nThank you!";
+        displayMessage(priceMessage);
+    }
+
+    public void clearOrder(View view) {
+        quantity = 0;
         display(quantity);
-        displayPrice(quantity * 5);
+        displayPrice(0);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(
-                R.id.quantity_text_view);
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
     /**
